f3566edf0571539255206eec8c0ef6073dd552b3
Kavish Devar <developer@sendildevar.in>
2024-07-09 03:07:56 +0530

    clean up demo code for app

--- app/src/main/java/me/kavishdevar/openrgb/MainActivity.kt
+++ app/src/main/java/me/kavishdevar/openrgb/MainActivity.kt
@@ -20,28 +20,9 @@ class MainActivity : ComponentActivity() {
         setContent {
             OpenRGBTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    Text("Hi!", modifier = Modifier.padding(innerPadding))
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    OpenRGBTheme {
-        Greeting("Android")
     }
 }
