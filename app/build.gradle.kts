487f248fbed51ccaf89ce71039828864a4d9980f
Kavish Devar <developer@sendildevar.in>
2024-07-09 14:04:32 +0530

    Add colorpicker-compose dep

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,6 +61,7 @@ dependencies {
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
     implementation(libs.openrgb.client)
+    implementation(libs.colorpicker.compose)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
